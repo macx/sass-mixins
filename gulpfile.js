e1cf281f7364a4bf0e468800eab3076a19f051cb
David Maciejewski <macx@macx.de>
2015-01-14 11:14:34 +0100

    feat(gulp): add gulp serve task

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,15 @@ var conf = {
   test: 'test'
 };
 
-var gulp   = require('gulp'),
-    sass   = require('gulp-sass'),
-    bump   = require('gulp-bump'),
-    git    = require('gulp-git'),
-    jshint = require('gulp-jshint');
+var gulp = require('gulp'),
+    sass        = require('gulp-sass'),
+    cssmin      = require('gulp-cssmin'),
+    rename      = require('gulp-rename'),
+    bump        = require('gulp-bump'),
+    git         = require('gulp-git'),
+    jshint      = require('gulp-jshint'),
+    browserSync = require('browser-sync'),
+    reload      = browserSync.reload;
 
 var getPackageJson = function () {
   var fs = require('fs');
@@ -27,7 +31,24 @@ gulp.task('lint', function () {
 gulp.task('sass', function () {
   gulp.src(conf.src + '/*.scss')
     .pipe(sass())
+    .pipe(gulp.dest(conf.test + '/'))
+    .pipe(cssmin())
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('cssmin', function () {
+  gulp.src([conf.test + '/*.css', '!' + conf.test + '/*.min.css'])
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest(conf.test + '/'));
+});
+
+gulp.task('browser-sync', function () {
+  browserSync({
+    server: {
+      baseDir: 'test/'
+    }
+  });
 });
 
 /**
@@ -71,6 +92,12 @@ gulp.task('release', ['default'], function () {
 // Default Task
 gulp.task('default', [
   'lint',
-  'sass'
+  'sass',
+  'cssmin'
+]);
+
+gulp.task('serve', [
+  'default',
+  'browser-sync'
 ]);
 
