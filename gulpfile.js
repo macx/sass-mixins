535869aff6c43d907c28c9f550ba4cc64bea1ca2
David Maciejewski <macx@macx.de>
2015-07-09 11:31:45 +0200

    fix linting errors and cleaned up the project

--- gulpfile.js
+++ gulpfile.js
@@ -5,55 +5,51 @@ var conf = {
   test: 'test'
 };
 
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    cssmin      = require('gulp-cssmin'),
-    rename      = require('gulp-rename'),
-    bump        = require('gulp-bump'),
-    git         = require('gulp-git'),
-    jshint      = require('gulp-jshint'),
-    scsslint    = require('gulp-scss-lint'),
-    browserSync = require('browser-sync'),
-    reload      = browserSync.reload;
+var gulp = require('gulp');
+var debug = require('gulp-debug');
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var jshint = require('gulp-jshint');
+var scsslint = require('gulp-scss-lint');
+var scssLintStylish = require('gulp-scss-lint-stylish');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 require('gulp-release-tasks')(gulp);
 
-var getPackageJson = function () {
-  var fs = require('fs');
-
-  return JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-};
-
 gulp.task('js-lint', function () {
   return gulp.src(['gulpfile.js'])
+    .pipe(debug({title: ' @js-lint:'}))
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('scss-lint', function() {
   return gulp.src([conf.src + '/**/*.scss'])
+    .pipe(debug({title: ' @scss-lint:'}))
     .pipe(scsslint({
-      'bundleExec': false,
-      'config': '.scss-lint.yml',
-      'reporterOutput': null
-    }))
-    .pipe(scsslint.failReporter());
+      maxBuffer: 307200,
+      config: '.scss-lint.yml',
+      customReport: scssLintStylish
+    }));
 });
 
 // Build SASS
-gulp.task('sass', function () {
+gulp.task('sass', function(done) {
   gulp.src(conf.src + '/*.scss')
-    .pipe(sass())
+    .pipe(debug({title: ' @sass:'}))
+    .pipe(sass({
+      errLogToConsole: true
+    }))
     .pipe(gulp.dest(conf.test + '/'))
     .pipe(cssmin())
-    .pipe(reload({stream: true}));
-});
-
-gulp.task('cssmin', function () {
-  gulp.src([conf.test + '/*.css', '!' + conf.test + '/*.min.css'])
-    .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(conf.test + '/'));
+    .pipe(gulp.dest(conf.test + '/'))
+    .pipe(reload({
+      stream: true
+    }))
+    .on('end', done);
 });
 
 gulp.task('browser-sync', function () {
@@ -68,8 +64,7 @@ gulp.task('browser-sync', function () {
 gulp.task('default', [
   'js-lint',
   'scss-lint',
-  'sass',
-  'cssmin'
+  'sass'
 ]);
 
 gulp.task('serve', [
